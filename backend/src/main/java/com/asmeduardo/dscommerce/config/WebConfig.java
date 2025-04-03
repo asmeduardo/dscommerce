655b43eecc87762cfc567e35ffc86479f39d17a3
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-04-03 16:41:46 -0300

    Spring Security checklist, not tested

--- backend/src/main/java/com/asmeduardo/dscommerce/config/WebConfig.java
+++ backend/src/main/java/com/asmeduardo/dscommerce/config/WebConfig.java
@@ -1,11 +1,15 @@
 package com.asmeduardo.dscommerce.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,5 +34,15 @@ public class WebConfig {
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Profile("test")
+    @Order(1)
+    SecurityFilterChain h2SecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.securityMatcher(PathRequest.toH2Console()).csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
     }
 }
---
+++ backend/src/main/java/com/asmeduardo/dscommerce/services/UserService.java
@@ -0,0 +1,43 @@
+package com.asmeduardo.dscommerce.services;
+
+import com.asmeduardo.dscommerce.models.Role;
+import com.asmeduardo.dscommerce.models.User;
+import com.asmeduardo.dscommerce.projections.UserDetailsProjection;
+import com.asmeduardo.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.getFirst().getPassword());
+
+        for (UserDetailsProjection projection : result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
