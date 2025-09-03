d9e49412f8b919dcae18e9c93e649b0e5b9f807f
Eduardo Melo <eduardo.melo01992@gmail.com>
2025-09-03 15:23:43 -0300

    Fix validation

--- backend/src/main/java/com/asmeduardo/dscommerce/dtos/FieldMessageDTO.java
+++ backend/src/main/java/com/asmeduardo/dscommerce/dtos/FieldMessageDTO.java
@@ -1,4 +1,20 @@
 package com.asmeduardo.dscommerce.dtos;
 
-public record FieldMessageDTO(String fieldName, String message) {
+public class FieldMessageDTO {
+
+    private String fieldName;
+    private String message;
+
+    public FieldMessageDTO(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
